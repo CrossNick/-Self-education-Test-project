65a990bc375c0f7adcaca0555c57c63136688773
CrossNick <nagatopeih@yandex.ru>
2018-07-08 13:19:24 +0300

    Created basic project structure

---
+++ TestProjectBookStore/BS.Data/EntityModels/AuthorEM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.Data.EntityModels
+{
+    class AuthorEM
+    {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BooksCount { get; set; }
+    }
+}
---
+++ TestProjectBookStore/BS.Data/EntityModels/BookEM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.Data.EntityModels
+{
+    class BookEM
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public IEnumerable<AuthorEM> Authors { get; set; }
+        public int Rating { get; set; }
+        public int PageCount { get; set; }
+    }
+}
